a181c48159a6a9607e8fe2b21f5745723dda12bf
wttttt <891570676@qq.com>
2024-05-28 15:59:45 +0800

    增加中文名称

---
+++ 测试程序/1.模拟下位机与电机通信/HARDWARE/PWM/PWM.h
@@ -0,0 +1,12 @@
+#ifndef __PWM_H
+#define __PWM_H
+
+#include "stm32f4xx.h"                  // Device header
+
+void PWM_Init(uint32_t arr, uint16_t psc, uint32_t ccr);                      //使用TIM2通道2输出PWM波
+void PWM_SetCompare1(uint16_t compare);   //修改占空比   修改比较寄存器CCR的值 
+   
+
+
+
+#endif 
---
+++ 测试程序/1.模拟下位机与电机通信/HARDWARE/Sensor/Sensor.h
@@ -0,0 +1,13 @@
+#ifndef __SENSOR_H
+#define __SENSOR_H
+
+#include "stm32f4xx.h"
+#include "led.h"
+#include "delay.h"
+#include "timer.h"
+#include "usart.h"
+
+void Sensor_Init(void);
+void Sensor1_Init(void);
+
+#endif
